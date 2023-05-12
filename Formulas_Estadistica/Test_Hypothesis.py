a7ab90fc57142ee3d0d17cceb8d7912d8a187341
David Andrés Valencia García <109925924+devBorisG@users.noreply.github.com>
2023-05-12 12:03:44 -0500

    commit

--- Formulas_Estadistica/Test_Hypothesis.py
+++ Formulas_Estadistica/Test_Hypothesis.py
@@ -12,14 +12,13 @@ Pasos Generales
 8. Decidir si se debe rechazar o no Ho y contextualizar la decisión del problema
 """
 import math
-from statistics import mean
 
 from scipy.stats import norm
 
 
 class Average(object):
     def __init__(self, x_barra: float, miu: float, alpha: float, sigma1: float, sigma2: float, population: int,
-                 sample: int):
+                 sample: int, respuesta: str):
 
         self.x_barra = x_barra
         self.miu = miu
@@ -28,37 +27,68 @@ class Average(object):
         self.sigma2 = sigma2
         self.population = population
         self.sample = sample
+        self.respuesta = respuesta
 
     @classmethod
-    def case_l_with_data(cls, data: list, sample: int, sigma1: float, sigma2: float) -> object:
+    def case_l(cls, x_barra: float, population: int, sample: int, sigma1: float, sigma2: float, miu: float,
+               alpha: float, option: int):
         """
-        The case_l_with_data function is a class method that creates an instance of the case_l class.
-        It takes in four arguments: data, sample, sigma 1 and sigma 2. The data argument is a list of numbers
-        hat represent the population from which we will take our sample. The sample argument represents how many
-        numbers we want to take from our population (sample size). Sigma 1 and sigma 2 are two different standard deviations
-        of the same normal distribution.
+        The case_l function is used to create an instance of the CaseLWithData class.
+        It takes in a number of parameters and returns an object with those parameters as attributes.
+        The function also calls the hypothesis_with_z method, which calculates whether Ho should be rejected.
 
-        :param cls: Create a new instance of the class
-        :param data: list: Pass the data to the class
-        :param sample: int: Specify the sample size
-        :param sigma1: float: Set the value of the sigma_one attribute
-        :param sigma2: float: Calculate the standard deviation of the sample
-        :return: An object of class case_l
+        :param cls: Refer to the class that is being instantiated
+        :param x_barra: float: Pass the value of the sample mean
+        :param population: int: Represent the population size
+        :param sample: int: Determine the sample size
+        :param sigma1: float: Calculate the standard deviation of the population
+        :param sigma2: float: Calculate the z-score
+        :param miu: float: Represent the mean of a population
+        :param alpha: float: Determine the alpha level
+        :param option: int: User option to appy the test (upper tail [Option 1], lower tail[Option2] or both[Option 3])
+        :return: An instance of the class case_l
         :doc-author: David
         """
-        case_l_with_data = cls.__new__(cls)
-        case_l_with_data.x_barra = mean(data)
-        case_l_with_data.population = len(data)
-        case_l_with_data.sample = sample
-        case_l_with_data.sigma1 = sigma1
-        case_l_with_data.sigma2 = sigma2
-        return case_l_with_data
 
-    def hypothesis_with_z(self):
-        z = (self.x_barra - self.miu) / (
-            math.sqrt(((self.sigma1 ** 2) / self.population) + ((self.sigma2 ** 2) / self.sample)))
-        z_alpha = norm.ppf(1 - self.alpha)
-        if z > z_alpha:
-            return "Se rechaza Ho"
-        else:
-            return "No se rechaza Ho"
+        def __hypothesis_with_z(self) -> bool:
+            """
+            The hypothesis_with_z function is used to determine if the null hypothesis should be rejected or not.
+            It uses the z-score and compares it with a critical value, which is calculated using the alpha level and
+            the cumulative distribution function of a normal distribution. If z &gt; z_alpha, then Ho will be rejected.
+
+            :param self: Allow an instance of a class to refer to itself
+            :return: The result of the hypothesis test,
+            :doc-author: David
+            """
+            z = (self.x_barra - self.miu) / (
+                math.sqrt(((self.sigma1 ** 2) / self.population) + ((self.sigma2 ** 2) / self.sample)))
+            z_alpha = norm.ppf(1 - self.alpha)
+            if option is 1:
+                if z > z_alpha:
+                    return True
+                else:
+                    return False
+            elif option is 2:
+                if z < z_alpha:
+                    return True
+                else:
+                    return False
+            elif option is 3:
+                # TODO: Preguntar como es la decisión en este caso y el calculo
+                pass
+
+        case_l = cls.__new__(cls)
+        case_l.x_barra = x_barra
+        case_l.population = population
+        case_l.sample = sample
+        case_l.sigma1 = sigma1
+        case_l.sigma2 = sigma2
+        case_l.miu = miu
+        case_l.alpha = alpha
+        case_l.respuesta = "Se rechaza Ho" if __hypothesis_with_z(case_l) else "No se rechaza Ho"
+        return case_l
+
+    @classmethod
+    def case_ll(cls, x_barra: float, population: int, sample: int, sigma1: float, sigma2: float, miu: float,
+                alpha: float, option: int):
+        pass
